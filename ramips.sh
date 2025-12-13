0a0db8424555646fd5bdd141e30c2d8fe6f76eef
robinquca <155705374+robinquca@users.noreply.github.com>
2025-12-13 22:45:44 +0800

    Update ramips.sh

--- ramips.sh
+++ ramips.sh
@@ -9,7 +9,7 @@ function git_sparse_clone() {
 }
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 # 添加额外插件
 #git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash package/luci-app-openclash
